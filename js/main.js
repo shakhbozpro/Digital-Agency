96d01ba4c0f723cf8a452af954a3e8c1ceda7cc5
shakhbozpro <shakhbozmamatkulov111@gmail.com>
2022-10-03 21:05:57 +0500

    Finished details, services and news sections with it's responsive

--- js/main.js
+++ js/main.js
@@ -4,4 +4,29 @@ let navMenyu = document.querySelector(".nav");
 burgerBtn.addEventListener("click" , () =>{
     burgerBtn.classList.toggle("shownav");
     navMenyu.classList.toggle("shownav");
-})
+});
+
+$('.slider-list').slick({
+    centerMode: true,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 1000,
+            settings: {
+                centerMode: true,
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                centerMode: true,
+                slidesToShow: 1
+            }
+        }
+    ]
+});
